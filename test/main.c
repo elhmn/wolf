d89880634029ce0eb7882f4b0287383fc115437f
elhmn <elhmn@localhost.localdomain>
2014-11-24 17:18:28 +0100

    color

---
+++ test/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <mlx.h>
+#include <stdlib.h>
+
+#define WIDTH 400
+#define	HEIGHT 400
+
+int		main(void)
+{
+	void*			mlx;
+	void*			win;
+	void*			img;
+	int				bpp;
+	unsigned int	color_value;
+	char			*image;
+	int				line;
+	int				endian;
+
+	int				mask_r = 0xff0000;
+	int				mask_g = 0x00ff00;
+	int				mask_b = 0x0000ff;
+
+	int				color = 0x70082f;
+
+	img = NULL;
+	win = NULL;
+	mlx = NULL;
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, WIDTH, HEIGHT, "42");
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	win = win;
+	color_value = mlx_get_color_value(mlx, 0xFFFF00);
+	printf("color_value = [%d]\n", color_value);
+	image = mlx_get_data_addr(img, &bpp, &line, &endian);
+	printf("bpp = [%d] && line = [%d] && endian = [%d]\n", bpp, line, endian);
+	int		i;
+	for (i = 0; i < 20000; i++)
+	{
+		image[i++] = mlx_get_color_value(mlx, mask_b & color); //bleu
+		image[i++] = mlx_get_color_value(mlx, ((mask_g & color) >> 8)); //green
+		image[i++] = mlx_get_color_value(mlx, ((mask_r & color) >> (8 * 2))); //red
+		image[i] = mlx_get_color_value(mlx, 0x000000); //alpha
+	}
+	mlx_put_image_to_window(mlx, win, img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
---
+++ test/pixel_put_on_image.c
@@ -0,0 +1,16 @@
+#include <mlx.h>
+#include <stdlib.h>
+
+int		mlx_pixel_put_img(void *mlx, void *img, char *img_addr, int color, int x, int y)
+{
+	int	r;
+	int	g;
+	int	b;
+	int	a;
+
+	r = ;
+	g = ;
+	b = ;
+	a = ;
+	return (0);
+}
